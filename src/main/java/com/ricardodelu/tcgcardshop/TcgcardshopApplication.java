28fce1a3263dd5d0b95a703a1cb0adb399b1581b
ricardodelu <riluso@gmail.com>
2025-07-22 17:51:59 -0600

    initialize Spring Boot application with basic configuration and Maven wrapper

---
+++ src/main/java/com/ricardodelu/tcgcardshop/TcgcardshopApplication.java
@@ -0,0 +1,13 @@
+package com.ricardodelu.tcgcardshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TcgcardshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TcgcardshopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ricardodelu/tcgcardshop/TcgcardshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ricardodelu.tcgcardshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TcgcardshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
